0f4e877db3d4a9a39ad2bc251fe510e9a8ce7913
faizanrozani <farozani@gmail.com>
2023-01-26 11:57:22 -0600

    Templates!

--- students/models.py
+++ students/models.py
@@ -1,13 +1,12 @@
-from django.db import models
 from django.contrib.auth.models import User
-
+from django.db import models
 
 # Create your models here.
+
 class Student(models.Model):
     first_name = models.CharField(max_length=60)
     last_name = models.CharField(max_length=60)
-    user = models.OneToOneField(User, null=True, 
-    on_delete=models.CASCADE, primary_key=False)
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE, primary_key=False)
 
     def __str__(self) -> str:
         return self.first_name + " " + self.last_name
@@ -15,7 +14,7 @@ class Student(models.Model):
 class Course(models.Model):
     department = models.CharField(max_length=60)
     number = models.CharField(max_length=3)
-    students = models.ManyToManyField(Student)
+    students = models.ManyToManyField(Student, related_name='student_courses')
 
     def __str__(self) -> str:
         return self.department + "-" + self.number
